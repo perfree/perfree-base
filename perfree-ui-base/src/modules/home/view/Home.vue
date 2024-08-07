79ae80f28954eb259a0b4e11220513224d530887
perfree <perfree@126.com>
2024-08-07 10:37:27 +0800

    echarts

--- perfree-ui-base/src/modules/home/view/Home.vue
+++ perfree-ui-base/src/modules/home/view/Home.vue
@@ -1,22 +1,132 @@
 <template>
- <div>
-   <div>home</div>
-   <el-button>Default</el-button>
-   <el-button type="primary">Primary</el-button>
-   <el-button type="success">Success</el-button>
-   <el-button type="info">Info</el-button>
-   <el-button type="warning">Warning</el-button>
-   <el-button type="danger">Danger</el-button>
-   <div>我改了改2222</div>
-   <el-alert title="Success alert" type="success" />
-   <el-alert title="Info alert" type="info" />
-   <el-alert title="Warning alert" type="warning" />
-   <el-alert title="Error alert" type="error" />
+  <el-row :gutter="10">
+    <el-col :span="18">
+      <el-row :gutter="10">
+        <el-col :span="6">
+          <div class="panelBox">
+            <el-statistic title="Daily active users" :value="268500" />
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="panelBox">
+            <el-statistic title="Daily active users" :value="268500" />
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="panelBox">
+            <el-statistic title="Daily active users" :value="268500" />
+          </div>
+        </el-col>
+        <el-col :span="6">
+          <div class="panelBox">
+            <el-statistic title="Daily active users" :value="268500" />
+          </div>
+        </el-col>
+        <el-col :span="24">
+          <div  class="panelBox" id="main" style="height:500px;"></div>
+        </el-col>
+      </el-row>
+    </el-col>
+    <el-col :span="6">
+      <el-row :gutter="10">
+        <el-col :span="24">
+          <div class="panelBox" style="height:220px;">
+            快捷功能
+          </div>
+        </el-col>
 
- </div>
+        <el-col :span="24">
+          <div class="panelBox" style="height:220px;">
+            相关文档
+          </div>
+        </el-col>
+
+        <el-col :span="24">
+          <div class="panelBox" style="height:220px;">
+            最新插件
+          </div>
+        </el-col>
+
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <script setup>
+import * as echarts from 'echarts';
+import {onMounted} from "vue";
 
+onMounted(() => {
+  const primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--el-color-primary').trim();
+  let myChart = echarts.init(document.getElementById('main'));
+  myChart.setOption({
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#6a7985'
+        }
+      }
+    },
+    legend: {
+      data: ['访问量', 'Union Ads']
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: '访问量',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [120, 132, 101, 134, 90, 230, 210]
+      },
+      {
+        name: 'Union Ads',
+        type: 'line',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: [220, 182, 191, 234, 290, 330, 310]
+      },
+    ]
+  });
+})
 
 </script>
+<style scoped>
+.panelBox{
+  background: var(--el-bg-color);
+  padding: 15px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  width: calc(100% - 30px);
+}
+</style>
